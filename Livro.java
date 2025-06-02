50d7494d07aeb6c3af93ce4a302ec1a8a0a27bb1
Asafe <asafe.rodrigues@cs.udf.edu.br>
2025-06-01 21:35:21 -0300

    Update do codigo de livro

--- Livro.java
+++ Livro.java
@@ -1,65 +1,86 @@
-import java.time.LocalDate;
+import java.util.Scanner;
 
 public class Livro {
-    // Atributos
+
+    //Atributos
     private int isbn;
     private String titulo;
     private String autor;
-    private LocalDate data;
     private String editora;
-    private boolean disponivel;
-    
+    private String data;
+    private boolean status;
+
     //Métodos
-    public Livro(int isbn, String titulo, String autor, LocalDate data, String editora) {
+
+    //Construtor
+    public Livro(int isbn, String titulo, String autor, String editora, String data) {
         this.isbn = isbn;
         this.titulo = titulo;
         this.autor = autor;
-        this.data = data;
         this.editora = editora;
-        this.disponivel = true;
+        this.data = data;
+        this.status = true;
     }
 
-    
+    // Getters e Setters
     public int getIsbn() {
         return isbn;
-    }
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public String getAutor() {
-        return autor;
-    }
-
-    public LocalDate getData() {
-        return data;
-    }
-
-    public String getEditora() {
-        return editora;
     }
 
     public void setIsbn(int isbn) {
         this.isbn = isbn;
     }
 
+    public String getTitulo() {
+        return titulo;
+    }
+
     public void setTitulo(String titulo) {
         this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
     }
 
     public void setAutor(String autor) {
         this.autor = autor;
     }
 
-    public void setData(LocalDate data) {
-        this.data = data;
+    public String getEditora() {
+        return editora;
     }
 
     public void setEditora(String editora) {
         this.editora = editora;
     }
-    
-    
-    
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    // Representação
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "isbn=" + isbn +
+                ", titulo='" + titulo + '\'' +
+                ", autor='" + autor + '\'' +
+                ", editora='" + editora + '\'' +
+                ", data='" + data + '\'' +
+                ", status=" + status +
+                '}';
+    }
 }
